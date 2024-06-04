cee2ce8c3d110d146c433881c653d06e103647a6
Kevin <porkevin743@gmail.com>
2024-06-04 07:52:22 -0500

    Arreglos

---
+++ examenArreglos/src/main/java/listasenlazadas/examenarreglos/ExamenArreglos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package listasenlazadas.examenarreglos;
+
+public class ExamenArreglos {
+
+    public static void main(String[] args) {
+        Metodos m = new Metodos();
+        m.ingresarEnArreglo();
+        m.llenarArregloC();
+        m.imprimir();
+    }
+}
